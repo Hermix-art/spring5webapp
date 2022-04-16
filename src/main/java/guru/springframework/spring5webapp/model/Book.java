e36e651e385bcd4b89a2534ff602757db99f3377
herman_kulik <herman_kulik@epam.com>
2022-04-16 19:17:31 +0200

    add JPA model entities and repositories

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,88 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author Herman Kulik
+ */
+@Entity
+public class Book { // pojo
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String isbn;
+    private String title;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_book"))
+    private Set<Author> authors;
+
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, Set<Author> authors) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
